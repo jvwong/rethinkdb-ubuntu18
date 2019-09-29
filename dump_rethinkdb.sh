09e2fbf4f2ce10f76a685414fc815ebcbb25ed83
jvwong <jvwong@gmail.com>
2019-09-29 15:44:01 -0400

    Allow output file name

--- dump_rethinkdb.sh
+++ dump_rethinkdb.sh
@@ -4,15 +4,17 @@
 #   dump_rethinkdb
 #
 # SYNOPSIS:
-#   dump_rethinkdb.sh -n name -e export -f file 
+#   dump_rethinkdb.sh -c container -e export -n name -f file 
 #
 # DESCRIPTION:
 #   Dump data within Rethinkdb Docker container to archive. Assume that dump happens in /data.
 #   Used with Docker version 18.09.6
-#   -n name (required)
-#     The name of the container
+#   -c container (required)
+#     The container name 
 #   -e export 
 #     Limit the dump to the given database and/or table; Use dot notation e.g. 'test.authors'
+#   -n name (optional)
+#     The dump archive name; .tar.gz will be appended
 #   -f file (optional)
 #     Output to the specified host path
 
@@ -21,13 +23,14 @@ TIMESTAMP=`date "+%Y%m%d_%H%M%S"`
 ARCHIVE_OUTPUT_DIRECTORY=$(pwd)
 RETHINKDB_DATA_DIRECTORY='/data'
 ################################# OPTS #################################
-nflag=
-eflag=
-fflag=
-while getopts 'n:e:f:' OPTION
+cval=
+eval=
+nval=
+fval=
+while getopts 'c:e:n:f:' OPTION
 do
   case $OPTION in
-    n)  nflag=1
+    c)  cval=1
         CONTAINER_NAME="$OPTARG"
         if ! docker container ls --format ‘{{.Names}}’ | grep -wq "${CONTAINER_NAME}"; then
           printf 'Option -n "%s" is not a container\n' "${CONTAINER_NAME}"
@@ -36,11 +39,19 @@ do
         DUMP_ARCHIVE_NAME=${CONTAINER_NAME}_dump_${TIMESTAMP}.tar.gz
         ;;
 
-    e)  eflag=1
+    e)  eval=1
         DB_TABLE="$OPTARG"
+        if [ ! "$eval" ]; then
+          printf 'Option -e "%s" is required\n' "$oval"
+          exit 2
+        fi
         ;;
         
-    f)  fflag=1
+    n)  nval=1
+        DUMP_ARCHIVE_NAME="$OPTARG".tar.gz
+        ;;
+
+    f)  fval=1
         ARCHIVE_OUTPUT_DIRECTORY="$OPTARG"
         if [ ! -d "${ARCHIVE_OUTPUT_DIRECTORY}" ]; then
           printf 'Option -f "%s" is not a directory\n' ${ARCHIVE_OUTPUT_DIRECTORY}
@@ -48,7 +59,7 @@ do
         fi
         ;;
 
-    ?)  printf "Usage: %s -n name -e export [-f file]\n" $0 >&2
+    ?)  printf "Usage: %s -c container -e export [-n name] [-f file]\n" $0 >&2
         exit 2
         ;; 
 
@@ -58,11 +69,11 @@ done
 
 ################################ BACKUP #################################
 # Create gzip archives from data in volumes 
-if [ "$nflag" -a "$eflag" ]; then
+if [ "$cval" -a "$eval" ]; then
   docker exec -it ${CONTAINER_NAME} /bin/bash -c "rethinkdb dump -e ${DB_TABLE} -f ${DUMP_ARCHIVE_NAME}"
   docker cp ${CONTAINER_NAME}:${RETHINKDB_DATA_DIRECTORY}/${DUMP_ARCHIVE_NAME} ${ARCHIVE_OUTPUT_DIRECTORY}
   docker exec -it ${CONTAINER_NAME} /bin/bash -c "rm ${RETHINKDB_DATA_DIRECTORY}/${DUMP_ARCHIVE_NAME}"  
 else 
-  printf "Usage: %s -n name -e export [-f file]\n" $0 >&2
+  printf "Usage: %s -c container -e export [-n name] [-f file]\n" $0 >&2
   exit 2
 fi
